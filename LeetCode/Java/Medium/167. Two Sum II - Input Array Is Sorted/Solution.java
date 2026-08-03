f69877a7ac093f17237697727867dc0d29ae7408
Uzma Yasmeen <uzmayasmeen0911@gmail.com>
2026-08-03 21:21:01 +0530

    Add Medium Java Solution from LeetCode

---
+++ LeetCode/Java/Medium/167. Two Sum II - Input Array Is Sorted/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int i =0,j=1;
+        for(int k =0;k<nums.length-1;k++)
+        {
+            if(nums[i]+nums[j]==target) return new int[]{i+1,j+1};
+            else if(nums[i]+nums[j]<target) j++;
+            else i++;
+        }
+        return new int[]{};
+    }
+}
