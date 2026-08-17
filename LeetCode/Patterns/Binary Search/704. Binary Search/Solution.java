c2a503e27f5c6ae87aa5f94f1d599304a5662df4
Uzma Yasmeen <uzmayasmeen0911@gmail.com>
2026-08-17 17:13:20 +0530

    Add Java Solution to Patterns/Binary Search

--- LeetCode/Patterns/Binary Search/704. Binary Search/Solution.java
+++ LeetCode/Patterns/Binary Search/704. Binary Search/Solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start = 0, end = nums.length;
+        int start = 0, end = nums.length-1;
         while(start<=end)
         {
             int mid = (start+end)/2;
