1a9d5e5a3d17ecfdac612696b41955965d5ea962
Uzma Yasmeen <uzmayasmeen0911@gmail.com>
2026-08-31 16:55:08 +0530

    Add Easy Java Solution for 141. Linked List Cycle from LeetCode

--- LeetCode/GRIND_75.md
+++ LeetCode/GRIND_75.md
@@ -3,7 +3,7 @@
 Track your progress on the Grind 75 coding interview preparation list.
 
 ## Progress
-- **Completed:** 1 / 75 (1.3%)
+- **Completed:** 2 / 75 (2.7%)
 
 ---
 
@@ -60,7 +60,7 @@ Track your progress on the Grind 75 coding interview preparation list.
 
 ### 📂 Linked List
 - [ ] Reverse Linked List
-- [ ] Linked List Cycle
+- [x] [Linked List Cycle](./Java/Easy/141. Linked List Cycle/)
 - [ ] Merge Two Sorted Lists
 - [ ] Merge k Sorted Lists
 - [ ] Remove Nth Node From End of List
---
+++ LeetCode/Java/Easy/141. Linked List Cycle/Solution.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode temp = head;
+        int pos=-1;
+        while(temp!=null)
+        {
+            if(fast == slow && pos>=0) return true;
+            pos++;
+            temp = temp.next;
+        }
+        return false;
+    }
+}
